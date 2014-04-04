c69243c2b557a28e01bb65832cbc3ddd11a000e5
Avi Kohn <avi@ichro.me>
2014-04-04 11:07:13 -0400

    Added a Gmail widget and fixed Now padding

--- js/widgets.js
+++ js/widgets.js
@@ -4942,6 +4942,44 @@ var Widgets = {
 
 			this.utils.render(this.data);
 		}
+	},
+	25: {
+		id: 25,
+		size: 4,
+		order: 4.5,
+		name: "Gmail",
+		nicename: "gmail",
+		sizes: ["variable"],
+		desc: "Displays your Gmail inbox where you can manage your email.",
+		settings: [
+			{
+				type: "text",
+				nicename: "user",
+				label: "Account ID",
+				help: "If you're signed into multiple accounts, this is the \"authuser=\" value.<br /><br />For example, if you're signed into two accounts, jsmith1@gmail.com and jsmith2@gmail.com, the \"authuser\" value for jsmith2@gmail.com would be 1 since it's the second account (counting from zero) that you're signed into.",
+				placeholder: "Your \"authuser=\" value"
+			},
+			{
+				type: "number",
+				label: "Widget Height",
+				nicename: "height",
+				min: 100,
+				max: 800
+			}
+		],
+		config: {
+			user: "0",
+			height: 400,
+			size: "variable"
+		},
+		render: function() {
+			this.utils.render({
+				user: this.config.user || 0,
+				height: this.config.height || 400
+			});
+
+			this.elm.addClass("tabbed");
+		}
 	}
 };
 
