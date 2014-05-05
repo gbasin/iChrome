0bcf72275b4237d71a02a138f77e1ccdf5fe4f10
Avi Kohn <avi@ichro.me>
2014-05-05 12:32:37 -0400

    Added a new tab option to the top sites widget

--- js/widgets.js
+++ js/widgets.js
@@ -1818,10 +1818,20 @@ var Widgets = {
 				label: "Number of sites shown",
 				min: 1,
 				max: 20
+			},
+			{
+				type: "radio",
+				nicename: "target",
+				label: "Open sites in",
+				options: {
+					_self: "The current tab",
+					_blank: "A new tab"
+				}
 			}
 		],
 		config: {
 			show: 5,
+			target: "_self",
 			size: "variable"
 		},
 		refresh: function() {
@@ -1834,6 +1844,7 @@ var Widgets = {
 				var sites = sites.slice(0, that.config.show),
 					data = {
 						title: that.config.title || false,
+						newTab: that.config.target == "_blank",
 						sites: sites
 					};
 
