2f94f2565fa509b5ad2a7152ae48b9824e7f2dd7
Avi Kohn <avi@ichro.me>
2014-05-28 13:52:58 -0400

    Relocate JS files and require everything

---
+++ app/app.js
@@ -0,0 +1,28 @@
+/**
+ * Configure requireJS.
+ */
+require.config({
+	baseUrl: "/js",
+	paths: {
+		"app": "/app",
+		"oauth2": "/oauth2/oauth2"
+	},
+	shim: {
+		"jquery.sortable": ["jquery"],
+		"jquery.numberformatter": ["jquery", "jshashtable"],
+		"jquery.spectrum": ["jquery"],
+
+		// This is temporarily in the shim till it's restructured
+		"app/script": ["moment", "app/storage", "app/widgets", "jquery.spectrum", "jquery.sortable"],
+		"app/widgets": ["jquery.numberformatter", "moment", "hogan", "oauth2"]
+	}
+});
+
+/**
+ * Load, this has to load moment this way because script.js needs to shimmed and moment doesn't define properly unless require'd
+ */
+require(["moment"], function(moment) {
+	window.moment = moment;
+
+	require(["app/script"]);
+});
