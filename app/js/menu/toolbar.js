gbasin/iChrome

commit fb3cda1cf84d4a2d026cb185ce8b2cc8a5a07c4f

Fixed menu button issues
If the menu style was changed from toolbar to button then the handlers
in both init.js and toolbar.js would be called.  init.js would move the
menu and toolbar.js would move it back immediately.

app/js/menu/toolbar.js
@@ -111,18 +111,24 @@ define(["lodash", "jquery", "backbone", "storage/storage", "storage/defaults", "
 
 
 			render: function() {
+				if (this.model.get("toolbar") == "full") {
+					this.Menu.$el.detach();
+				}
+
 				// The app icons attributes get reset on render
 				this.appsLoaded = false;
 
 				this.Search.$el.detach();
 
-				this.Menu.$el.detach();
-
 				this.$el.html(render("toolbar", this.model.toJSON()));
 
 				this.$(".search").replaceWith(this.Search.el);
 
-				this.$("nav.menu").replaceWith(this.Menu.el);
+				if (this.model.get("toolbar") == "full") {
+					this.$("nav.menu").replaceWith(this.Menu.el);
+
+					this.Menu.delegateEvents();
+				}
 
 				return this;
 			}

app/js/core/init.js
@@ -63,14 +63,14 @@ define(
 
 						if (this.MenuButton) this.MenuButton.$el.detach();
 
-						this.$el.prepend(this.Toolbar.el);
+						this.$el.prepend(this.Toolbar.render().el);
 					}
 					else {
 						if (!this.MenuButton) this.MenuButton = new MenuButton();
 
 						if (this.Toolbar) this.Toolbar.$el.detach();
 
-						this.$el.prepend(this.MenuButton.el);
+						this.$el.prepend(this.MenuButton.render().el);
 					}
 
 					this.$el.removeClass("unloaded").children(".loading").remove();

app/js/menu/button.js
@@ -11,7 +11,15 @@ define(["backbone", "menu/menu"], function(Backbone, Menu) {
 		},
 
 		initialize: function() {
-			this.$el.append('<div class="menu-button"></div>', Menu.$el);
+			this.render();
+		},
+
+		render: function() {
+			Menu.$el.detach();
+
+			this.$el.html('<div class="menu-button"></div>').append(Menu.el);
+
+			return this;
 		}
 	});
 

commit e4c0f6d60bfc219248da8886a4dd3bb6c0524974

Fixed a bug with the menu
The menu wasn't getting added to the DOM if you were using the normal
toolbar and have not changed your settings since 2.1.5.

This bug was caused by fb3cda1cf84d4a2d026cb185ce8b2cc8a5a07c4f

Since no releases are planned for the near future this requires one of
its own.

app/js/menu/toolbar.js
@@ -111,7 +111,9 @@ define(["lodash", "jquery", "backbone", "storage/storage", "storage/defaults", "
 
 
 			render: function() {
-				if (this.model.get("toolbar") == "full") {
+				var toolbar = this.model.get("toolbar") == "full" || this.model.get("toolbar") === true;
+
+				if (toolbar) {
 					this.Menu.$el.detach();
 				}
 
@@ -124,7 +126,7 @@ define(["lodash", "jquery", "backbone", "storage/storage", "storage/defaults", "
 
 				this.$(".search").replaceWith(this.Search.el);
 
-				if (this.model.get("toolbar") == "full") {
+				if (toolbar) {
 					this.$("nav.menu").replaceWith(this.Menu.el);
 
 					this.Menu.delegateEvents();