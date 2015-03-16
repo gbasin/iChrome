cbf2ada784d4b6e8f555acd60765a0c46f454613
Avi Kohn <avi@ichro.me>
2015-03-16 09:28:40 -0400

    Integrated enhanced search results from Wikiwand

---
+++ app/js/lib/wikiwand.js
@@ -0,0 +1,57 @@
+/**
+ * This is the Wikiwand search setup script
+ */
+define(function() {
+	var WikiwandSearch = function(params) {
+		if (typeof params == "undefined") {
+			return console.log("wikiwand search widget, no params specified");
+		}
+
+		if (!params.key) {
+			return console.log("wikiwand search widget - no key specified");
+		}
+		
+		if (!params.container) {
+			return console.log("wikiwand search widget - no container specified");
+		}
+		
+		params.cse = !!params.cse; // !! forces a boolean
+		params.autoFocus = !!params.autoFocus;
+
+		var container = typeof params.container === "string" ? document.getElementById(params.container) : params.container;
+
+		var frame = document.createElement("IFRAME");
+
+		var noConnection = params.noConnection || document.getElementById("noConnection");
+
+		frame.setAttribute("id", "wikiwand_searchframe");
+		frame.setAttribute("frameborder", "0");
+		frame.style.opacity = 0;
+		frame.style.width = "100%";
+		frame.style.height = "100%";
+		frame.setAttribute("src", "http://www.wikiwand.com/searchWidget?key=" + params.key + "&autoFocus=" + params.autoFocus + "&cse=" + params.cse);
+
+		container.style.overflow = "hidden";
+		container.style.backgroundColor = "white";
+		container.style.display = "block";
+
+		container.appendChild(frame);
+
+		if (params.autoFocus) {
+			frame.focus();
+		}
+
+		window.addEventListener("message", function(e) {
+			if (e && e.data && e.data.height) {
+				noConnection.style.display = "none";
+
+				frame.style.opacity = 1;
+
+				container.style.width = (e.data.width) + "px";
+				container.style.height = (e.data.height) + "px";
+			}
+		}, false);
+	};
+
+	return WikiwandSearch;
+});
--- app/js/search/search.js
+++ app/js/search/search.js
@@ -1,7 +1,7 @@
 /**
  * This handles the searchbox
  */
-define(["backbone", "storage/storage", "core/render", "core/analytics", "search/suggestions", "search/speech"], function(Backbone, Storage, render, Track, Suggestions, Speech) {
+define(["backbone", "wikiwand", "storage/storage", "core/render", "core/analytics", "search/speech"], function(Backbone, Wikiwand, Storage, render, Track, Speech) {
 	var Model = Backbone.Model.extend({
 			init: function() {
 				Storage.on("done updated", function(storage) {
@@ -13,61 +13,9 @@ define(["backbone", "storage/storage", "core/render", "core/analytics", "search/
 			tagName: "div",
 			className: "search",
 			events: {
-				"input input":		"change",
-				"click .speech":	"speech", // Backbone doesn't handle nested methods so this needs to be proxied
-				"keydown input":	"keydown",
-				"focusin input":	function(e) { this.change(e, "focusin"); },
-				"focusout input":	function(e) { this.change(e, "focusout"); }
-			},
-			keydown: function(e) {
-				if ([13, 38, 40].indexOf(e.which) !== -1) {
-					e.preventDefault();
-				}
-
-				if (e.which == 13) {
-					this.submit();
-				}
-				else if (e.which == 38) {
-					this.Suggestions.setFocus("prev");
-
-					Track.event("Search", "Suggestions", "Key Prev");
-				}
-				else if (e.which == 40) {
-					this.Suggestions.setFocus("next");
-
-					Track.event("Search", "Suggestions", "Key Next");
-				}
-			},
-			change: function(e, w) {
-				var val = e.currentTarget.value.trim();
-
-				if (val !== "" && w !== "focusout") {
-					this.Suggestions.load(val);
-
-					if (this.model.get("toolbar")) {
-						this.trigger("typing:start");
-					}
-				}
-				else if (!w || (w == "focusout")) { // If either w (which) isn't set or w is focusout; if w is not focusin
-					this.Suggestions.hide();
-
-					if (this.model.get("toolbar")) {
-						this.trigger("typing:end");
-					}
-				}
+				"click .speech": "speech"
 			},
 			submit: function(val) {
-				if (typeof val !== "string") {
-					val = this.$("input").val().trim();
-
-					Track.ga("send", {
-						useBeacon: true,
-						hitType: "pageview",
-						title: "Search: " + val,
-						page: "/search?q=" + encodeURIComponent(val)
-					});
-				}
-
 				var searchURL = (this.model.get("search-url") || "https://www.google.com/search?q=%s"),
 					link = document.createElement("a");
 
@@ -86,15 +34,6 @@ define(["backbone", "storage/storage", "core/render", "core/analytics", "search/
 				this.model = new Model();
 
 				this.Speech = new Speech();
-
-				this.Suggestions = new Suggestions();
-
-				// This is attached to both select and focuschange since searches won't happen instantly and it's good for users to see what they've selected
-				this.Suggestions.on("select focuschange", function(val) {
-					this.$("input").val(val);
-				}, this).on("select", function(val) {
-					this.submit(val);
-				}, this);
 
 				this.Speech.on("result", function(val) {
 					Track.ga("send", {
@@ -119,7 +58,13 @@ define(["backbone", "storage/storage", "core/render", "core/analytics", "search/
 						})
 					);
 
-				this.Suggestions.setElement(this.$(".suggestions"));
+				this.Wikiwand = new Wikiwand({
+					cse: false,
+					key: "slkjgops",
+					autoFocus: true,
+					container: this.$(".wikiwand")[0],
+					noConnection: this.$("input.noConnection")[0]
+				});
 
 				return this;
 			}
