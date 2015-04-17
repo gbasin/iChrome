00bc4a0511540712899dfc055d7278047a840b08
Avi Kohn <avi@ichro.me>
2015-04-17 13:32:49 -0400

    Added home and start page overrides to the manifest

--- app/js/background.js
+++ app/js/background.js
@@ -487,6 +487,18 @@ chrome.webRequest.onHeadersReceived.addListener(
 	["blocking", "responseHeaders"]
 );
 
+chrome.webRequest.onBeforeRequest.addListener(
+	function(info) {
+		return {
+			redirectUrl: "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/index.html"
+		};
+	},
+	{
+		urls: ["http://ichro.me/redirect"]
+	},
+	["blocking"]
+);
+
 
 chrome.webRequest.onAuthRequired.addListener(
 	function(info) {
