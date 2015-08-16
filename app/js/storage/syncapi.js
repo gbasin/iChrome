57f99e87815dcc4a8e941caae7d90db07f724d60
Avi Kohn <avi@ichro.me>
2015-08-16 14:20:21 -0400

    Syncing: Improved extension ID and version passing

--- app/js/storage/syncapi.js
+++ app/js/storage/syncapi.js
@@ -18,16 +18,8 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 	 * so clients under the same Chrome account are synced, and as a cookie at ichro.me
 	 * so they survive reinstalls or other local data erasures.
 	 */
-	var defaultData = {
-		user: {},
-		version: info.version,
-		extension: info.id
-	};
-
 	var clientData = {
-		user: {},
-		version: info.version,
-		extension: info.id
+		user: {}
 	};
 
 	try {
@@ -120,17 +112,17 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 			delete clientData.status;
 		}
 
-		localStorage.syncData = JSON.stringify(_.omit(clientData, "version", "extension"));
+		localStorage.syncData = JSON.stringify(clientData);
 
 		chrome.storage.sync.set({
-			syncData: _.omit(clientData, "client", "version", "extension")
+			syncData: _.omit(clientData, "client")
 		});
 
 		chrome.cookies.set({
 			name: "sync_data",
 			domain: ".ichro.me",
 			url: "http://ichro.me",
-			value: JSON.stringify(_.omit(clientData, "version", "extension")),
+			value: JSON.stringify(clientData),
 			expirationDate: (new Date().getTime() / 1000) + (10 * 365 * 24 * 60 * 60)
 		});
 	};
@@ -224,7 +216,11 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 			var get = function(retry) {
 				$.ajax({
 					url: syncBase + "/" + clientData.token,
-					data: _.assign(_.pick(clientData, "client", "version", "extension"), params),
+					data: _.assign({
+						extension: info.id,
+						version: info.version,
+						client: clientData.client
+					}, params),
 					timeout: 10000,
 					complete: function(xhr, status) {
 						if ((xhr.status === 200 && xhr.responseJSON) || xhr.status === 304) {
@@ -273,7 +269,9 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 							// If the error occurs again, then the issue must be in our sync profile
 							// so we will have to recreate it.
 							if (retry && xhr.status.toString()[0] !== "5") {
-								clientData = _.clone(defaultData);
+								clientData = {
+									user: {}
+								};
 
 								delete localStorage.syncData;
 
@@ -290,7 +288,9 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 								cb("Server error");
 							}
 							else {
-								clientData = _.clone(defaultData);
+								clientData = {
+									user: {}
+								};
 
 								delete localStorage.syncData;
 
@@ -359,7 +359,10 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 				$.ajax({
 					type: clientData.token && !code ? "PUT" : "POST",
 					url: url,
-					data: JSON.stringify(_.assign({}, data, clientData)),
+					data: JSON.stringify(_.assign({}, data, clientData, {
+						extension: info.id,
+						version: info.version
+					})),
 					contentType: "application/json",
 					timeout: 10000,
 					complete: function(xhr, status) {
@@ -410,7 +413,9 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 							// If the error occurs again, then the issue must be in our sync profile
 							// so we will have to recreate it.
 							if (retry && xhr.status.toString()[0] !== "5" && !(code && xhr.status === 401)) {
-								clientData = _.clone(defaultData);
+								clientData = {
+									user: {}
+								};
 
 								delete localStorage.syncData;
 
@@ -427,7 +432,9 @@ define(["jquery", "lodash", "core/analytics", "core/info"], function($, _, Track
 								cb("Server error");
 							}
 							else if (clientData.token) {
-								clientData = _.clone(defaultData);
+								clientData = {
+									user: {}
+								};
 
 								delete localStorage.syncData;
 
