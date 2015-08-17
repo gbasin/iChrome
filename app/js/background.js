8b7671ce757eedb28034fa60bf99df908a6fb71f
Avi Kohn <avi@ichro.me>
2015-08-17 14:30:14 -0400

    Removed the iChrome redirect webRequest handler
    This shouldn't be present in the new tab extension since it only applies
    to the main one.

--- app/js/background.js
+++ app/js/background.js
@@ -148,18 +148,6 @@ chrome.runtime.onInstalled.addListener(function(details) {
 	}
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
-	function(info) {
-		return {
-			redirectUrl: "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/index.html"
-		};
-	},
-	{
-		urls: ["http://ichro.me/redirect"]
-	},
-	["blocking"]
-);
-
 chrome.webRequest.onAuthRequired.addListener(
 	function(info) {
 		if (info.tabId !== -1 && info.scheme.toLowerCase().trim() === "basic") {
