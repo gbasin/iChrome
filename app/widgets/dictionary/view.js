2c267306c6137f273f721527c6398ec7e07707c8
Avi Kohn <avi@ichro.me>
2015-12-02 14:35:49 -0500

    Dictionary: Added support for web definitions
    In some cases, such as translating test when the user language is
    French, only web definitions are available.
    
    Screenshot: http://i.imgur.com/gutG4gb.png

--- app/widgets/dictionary/view.js
+++ app/widgets/dictionary/view.js
@@ -4,7 +4,7 @@ define(["lodash", "jquery", "browser/api", "widgets/views/main"], function(_, $,
 			"click button.more": function(e) {
 				var expanding = e.currentTarget.getAttribute("data-state") === "collapsed";
 
-				this.$(".forms, ol li .synonyms, ol li .antonyms, ol li:nth-child(n + 2), .usage:nth-of-type(n + 3)")[expanding ? "slideDown" : "slideUp"](300);
+				this.$(".forms, ol li .synonyms, ol li .antonyms, ol li:nth-child(n + 2), .usage:nth-of-type(n + 3), .usage ~ .web-definitions")[expanding ? "slideDown" : "slideUp"](300);
 
 				e.currentTarget.textContent = expanding ? "Less" : "More";
 				e.currentTarget.setAttribute("data-state", expanding ? "maximized" : "collapsed");
