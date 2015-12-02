7338dd27d2aa98bca8f9c2fc48beb6a1b571356c
Avi Kohn <avi@ichro.me>
2015-12-01 19:25:17 -0500

    Dictionary: Completed design
    Screenshots: http://i.imgur.com/VKJgHgK.png
    http://i.imgur.com/hvUr1AH.png

--- app/widgets/dictionary/model.js
+++ app/widgets/dictionary/model.js
@@ -1,7 +1,112 @@
 define(["jquery", "lodash", "widgets/model"], function($, _, WidgetModel) {
 	return WidgetModel.extend({
 		defaults: {
-			data: {}
+			data: {
+				definition: {
+					word: "test",
+					pronunciation: "test",
+					audio: "https://ssl.gstatic.com/dictionary/static/sounds/de/0/test.mp3",
+					uses: [{
+						form: "noun",
+						forms: [{
+							desc: "noun",
+							form: "test"
+						}, {
+							desc: "plural noun",
+							form: "tests"
+						}],
+						definitions: [{
+							definition: "a procedure intended to establish the quality, performance, or reliability of something, especially before it is taken into widespread use.",
+							example: "no sparking was visible during the tests",
+							synonymGroups: [{
+								synonyms: [{
+									text: "trial"
+								}, {
+									text: "experiment"
+								}, {
+									text: "test case"
+								}, {
+									text: "case study"
+								}, {
+									noDef: true,
+									text: "pilot study"
+								}, {
+									text: "trial run"
+								}, {
+									text: "tryout"
+								}, {
+									text: "dry run"
+								}]
+							}, {
+								register: "technical",
+								synonyms: [{
+									text: "assay"
+								}]
+							}],
+							antonymGroups: [{
+								antonyms: [{
+									text: "break"
+								}, {
+									text: "repair"
+								}]
+							}]
+						}, {
+							labels: ["Metallurgy"],
+							definition: "a movable hearth in a reverberating furnace, used for separating gold or silver from lead."
+						}]
+					}, {
+						form: "verb",
+						forms: [{
+							desc: "verb",
+							form: "test"
+						}, {
+							desc: "3rd person present",
+							form: "tests"
+						}, {
+							desc: "past tense",
+							form: "tested"
+						}, {
+							desc: "past participle",
+							form: "tested"
+						}, {
+							desc: "gerund or present participle",
+							form: "testing"
+						}],
+						definitions: [{
+							definition: "take measures to check the quality, performance, or reliability of (something), especially before putting it into widespread use or practice.",
+							example: "this range has not been tested on animals",
+							synonymGroups: [{
+								synonyms: [{
+									noDef: true,
+									text: "try out"
+								}, {
+									noDef: true,
+									text: "put to the test"
+								}, {
+									noDef: true,
+									text: "put through its paces"
+								}, {
+									noDef: true,
+									text: "experiment with"
+								}, {
+									text: "check"
+								}, {
+									text: "examine"
+								}, {
+									text: "assess"
+								}, {
+									text: "evaluate"
+								}]
+							}, {
+								register: "technical",
+								synonyms: [{
+									text: "assay"
+								}]
+							}],
+						}]
+					}]
+				}
+			}
 		},
 
 		refresh: function() {
--- app/widgets/dictionary/view.js
+++ app/widgets/dictionary/view.js
@@ -1,6 +1,14 @@
 define(["lodash", "jquery", "browser/api", "widgets/views/main"], function(_, $, Browser, WidgetView) {
 	return WidgetView.extend({
 		events: {
+			"click button.more": function(e) {
+				var expanding = e.currentTarget.getAttribute("data-state") === "collapsed";
+
+				this.$(".forms, ol li .synonyms, ol li .antonyms, ol li:nth-child(n + 2), .usage:nth-of-type(n + 3)")[expanding ? "slideDown" : "slideUp"](300);
+
+				e.currentTarget.textContent = expanding ? "Less" : "More";
+				e.currentTarget.setAttribute("data-state", expanding ? "maximized" : "collapsed");
+			}
 		},
 
 		onBeforeRender: function(data) {
