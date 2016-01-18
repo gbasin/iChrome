47a41bd8427b94ce9c64372f7ef04d6552112b09
Avi Kohn <avi@ichro.me>
2016-01-18 14:41:46 -0500

    Classroom & iFrame: Removed unnecessary code

--- app/widgets/classroom/maximized.js
+++ app/widgets/classroom/maximized.js
@@ -1,7 +1,5 @@
 define(["./view"], function(MainView) {
 	return MainView.extend({
-		onRender: function() {
-			this.$el.toggleClass("has-padding", this.model.config.padding === "true");
-		}
+		onRender: null
 	});
 });
