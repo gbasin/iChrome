ccb78ae547895fba806bbef05b1bd3344d091298
Avi Kohn <avi@ichro.me>
2016-05-31 11:12:36 -0400

    Reddit: Fixed issues with thumbnail parsing

--- app/widgets/reddit.js
+++ app/widgets/reddit.js
@@ -222,7 +222,7 @@ define(["jquery", "moment"], function($, moment) {
 						comments: e.num_comments.toLocaleString()
 					};
 
-					if (e.thumbnail && e.thumbnail !== "" && e.thumbnail.trim().indexOf("http") === "0") {
+					if (e.thumbnail && e.thumbnail !== "" && e.thumbnail.trim().indexOf("http") === 0) {
 						link.image = e.thumbnail.trim();
 					}
 
