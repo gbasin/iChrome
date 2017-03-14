87d903dd26bc7cffc2ae0ac00842640a1b57276a
Avi Kohn <avi@ichro.me>
2017-03-14 14:29:18 -0400

    Currency: Switched to Yahoo Finance based API

--- app/widgets/currency/model.js
+++ app/widgets/currency/model.js
@@ -31,9 +31,9 @@ define(["lodash", "jquery", "widgets/model"], function(_, $, WidgetModel) {
 				cb.call(ctx, this._cache[from + "-" + to] ? value * this._cache[from + "-" + to] : value / this._cache[to + "-" + from]);
 			}
 			else {
-				$.getJSON("https://rate-exchange.herokuapp.com/fetchRate?from=" + from + "&to=" + to, function(d) {
-					if (d && d.From === from && d.To === to && d.Rate) {
-						this._cache[from + "-" + to] = parseFloat(d.Rate);
+				$.getJSON('https://query.yahooapis.com/v1/public/yql?format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D"' + from + to + '"', function(d) {
+					if (d && d.query && d.query.results && d.query.results.rate && d.query.results.rate.Rate) {
+						this._cache[from + "-" + to] = parseFloat(d.query.results.rate.Rate);
 
 						cb.call(ctx, value * this._cache[from + "-" + to]);
 					}
