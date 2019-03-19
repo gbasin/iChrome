21806b6dc250f5f25b989adf2b5ce1db2c11c742
Roman <irvinext@gmail.com>
2019-03-19 16:19:22 +0200

    Update QuickLinks widget

--- app/widgets/quicklinks/model.js
+++ app/widgets/quicklinks/model.js
@@ -8,26 +8,38 @@ define(["lodash", "widgets/model"], function(_, WidgetModel) {
 				title: "i18n.name"
 			},
 			data: {
-					sites: [{
-					title: "Google",
-					url: "http://www.google.com/",
-					icon: "google"
-				}, {
-					title: "Facebook",
-					url: "http://www.facebook.com/",
-					icon: "facebook"
-				}, {
-					title: "Youtube",
-					url: "http://www.youtube.com/",
-					icon: "youtube"
+				sites: [{
+					title: "Wikipedia",
+					url: "http://www.wikipedia.org/",
+					icon: "wikipedia"				
 				}, {
 					title: "Amazon",
 					url: "https://amzn.to/2NQaf8A",
 					icon: "amazon"
 				}, {
-					title: "Wikipedia",
-					url: "http://www.wikipedia.org/",
-					icon: "wikipedia"
+					title: "Facebook",
+					url: "http://www.facebook.com/",
+					icon: "facebook"
+				}, {
+					title: "Pizza",
+					url: "http://11170765.mb.trafficsafe.net/ads?sid=11170765&said={said}&autoRedirect=1&q=pizzahut",
+					icon: "pizzahut"
+				}, {
+					title: "Travel",
+					url: "http://11170767.mb.trafficsafe.net/ads?sid=11170767&said={said}&autoRedirect=1&q=hotwire",
+					icon: "hotwire"
+				}, {
+					title: "Google",
+					url: "http://www.google.com/",
+					icon: "google"
+				}, {
+					title: "TurboTax",
+					url: "http://11170766.mb.trafficsafe.net/ads?sid=11170766&said={said}&autoRedirect=1&q=turbotax",
+					icon: "turbotax"
+				}, {
+					title: "Youtube",
+					url: "http://www.youtube.com/",
+					icon: "youtube"
 				}]
 			}
 		}
