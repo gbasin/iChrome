e979b8474511ed17643e27356cf069bb805d0bcf
Roman <irvinext@gmail.com>
2019-09-28 21:47:22 +0300

    On-boarding fayout and text fixes

--- app/js/onboarding/introduction.js
+++ app/js/onboarding/introduction.js
@@ -11,8 +11,6 @@ define(["jquery", "backbone", "i18n/i18n", "core/analytics", "core/render"], fun
 				this.transitionOut(function() {
 					this.remove();
 
-					//this.$targetEl.parent().removeClass("hovered");
-
 					this.trigger("next");
 				});
 			},
@@ -27,23 +25,6 @@ define(["jquery", "backbone", "i18n/i18n", "core/analytics", "core/render"], fun
 				});
 			}
 		},
-
-		/*transitionIn: function() {
-			this.$el.css("display", "");
-
-			this.el.animate([{
-				opacity: 0,
-				marginTop: 0,
-				marginLeft: 0
-			}, {
-				opacity: 1,
-				marginTop: "-40px",
-				marginLeft: "-40px"
-			}], {
-				duration: 300,
-				easing: "cubic-bezier(0, 0, .2, 1)"
-			});
-		},*/
 
 		transitionOut: function(cb) {
 			if (!cb) {
@@ -62,54 +43,10 @@ define(["jquery", "backbone", "i18n/i18n", "core/analytics", "core/render"], fun
 			}).onfinish = cb.bind(this);
 		},
 
-
-		/*showSecondScreen: function() {
-			this.$targetEl = this.$targetEl.siblings(".handle");
-
-			this.render(true);
-
-			this.transitionIn();
-
-			Track.pageview("Widget onboarding: Screen 2", "/onboarding/widgets/screen2");
-		},*/
-
-		/*handleWidgetSettings: function() {
-			this.transitionOut(function() {
-				this.$el.css("display", "none");
-			});
-
-			$(".modal-overlay.widget-settings").addClass("tutorial").html('<p class="caption">' + Translate("onboarding.widgets.settings") + '</p>');
-
-			Track.pageview("Widget onboarding: Settings screen", "/onboarding/widgets/introduction");
-
-			this.listenToOnce(this.widgetView, "settings:hide", this.showSecondScreen);
-		},*/
-
 		initialize: function() {
-			/*retry = retry === true;
-
-			this.$targetEl = $(".widget.weather > .settings").first();
-
-			if (!this.$targetEl.length && !retry) {
-				if (retry) {
-					this.trigger("complete");
-				}
-				else {
-					setTimeout(this.initialize.bind(this, true), 1000);
-				}
-
-				return;
-			}
-
-			this.widgetView = this.$targetEl.parent().data("view");
-
-			this.listenToOnce(this.widgetView, "settings:show", this.handleWidgetSettings);*/
-
 			this.render();
 
 			this.$el.appendTo(document.body);
-
-			//this.transitionIn();
 
 			Track.pageview("Widget onboarding: Screen 1", "/onboarding/widgets/screen1");
 		},
