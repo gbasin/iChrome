f07a5f0be577db2a1e78ec4772a0ee3b69987591
Roman <irvinext@gmail.com>
2020-05-22 21:29:07 +0300

    Keep widget: insert CSS without permissions request

---
+++ app/inject/framehelper.js
@@ -0,0 +1,5 @@
+var parentOrigin = location.ancestorOrigins[location.ancestorOrigins.length - 1];
+if (parentOrigin === location.origin) {
+    // Only send a message if the frame was opened by ourselves.
+    chrome.runtime.sendMessage(location.hash.slice(1));
+}
--- app/widgets/keep/view.js
+++ app/widgets/keep/view.js
@@ -5,6 +5,41 @@ define(["widgets/views/main"], function(WidgetView) {
 		onBeforeRender: function(data) {
 			data.url = "https://keep.google.com/u/" + (this.model.config.user || 0);
 
+			var randomMessage = 'idkeep' + Math.random();
+			data.randomMessage = randomMessage;
+
+			function injectInFrame(tabId, frameId) {
+				chrome.tabs.insertCSS(tabId, {
+					frameId,
+					file: "/inject/css/keep.css", 
+					allFrames: true
+				});
+			}
+
+			function frameMessageListener(msg, sender) {
+				if (msg !== randomMessage) return;
+				var tabId = sender.tab.id;
+				var frameId = sender.frameId;
+				var f = document.getElementById(randomMessage);
+
+				chrome.runtime.onMessage.removeListener(frameMessageListener);
+				// This will cause the script to be run on the first load.
+				// If the frame redirects elsewhere, then the injection can seemingly fail.
+				f.addEventListener('load', function onload() {
+					f.removeEventListener('load', onload);
+					injectInFrame(tabId, frameId);
+				});
+				f.src = data.url;
+
+				for (var i = 0; i < 20; i++) {
+					setTimeout(function() {
+						injectInFrame(tabId, frameId);
+					}, 100 * i);
+				}
+			}
+
+			chrome.runtime.onMessage.addListener(frameMessageListener);
+
 			return data;
 		},
 
